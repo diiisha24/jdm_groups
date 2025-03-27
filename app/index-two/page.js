0c596f7959bfb6fd78f3bbfa21e5d2f95970f953
Disha Garg <gargdisha1420@gmail.com>
2025-03-27 11:15:06 +0530

    APP Added.

--- app/index-two/page.js
+++ app/index-two/page.js
@@ -1,31 +1,33 @@
-import Layout from "@/components/layout/Layout";
-import About2 from "@/components/sections/About2";
-import Achivements from "@/components/sections/Achivements";
-import Brand2 from "@/components/sections/Brand2";
-import CtaBanner2 from "@/components/sections/CtaBanner2";
-import Hero3 from "@/components/sections/Hero3";
-import News2 from "@/components/sections/News2";
-import Pricing from "@/components/sections/Pricing";
-import Project2 from "@/components/sections/Project2";
-import Service2 from "@/components/sections/Service2";
-import Team2 from "@/components/sections/Team2";
-import Testimonial2 from "@/components/sections/Testimonial2";
-export default function HomeTwo() {
+import Layout from "@/components/layout/Layout"
+import About2 from "@/components/sections/About2"
+import Achivements from "@/components/sections/Achivements"
+import Brand1 from "@/components/sections/Brand1"
+import CtaBanner2 from "@/components/sections/CtaBanner2"
+import Hero2 from "@/components/sections/Hero2"
+import News2 from "@/components/sections/News2"
+// import Pricing from "@/components/sections/Pricing"
+import Project2 from "@/components/sections/Project2"
+// import Service2 from "@/components/sections/Service2"
+import Service3 from "@/components/sections/Service3"
+import Team2 from "@/components/sections/Team2"
+import Testimonial2 from "@/components/sections/Testimonial2"
+export default function Home2() {
   return (
     <>
-      <Layout headerStyle={4} footerStyle={2} breadcrumbTitle="">
-        <Hero3 />
+      <Layout headerStyle={2} footerStyle={2}>
+        <Hero2 />
         <About2 />
-        <Service2 />
-        <Brand2 />
+        {/* <Service2 /> */}
+        <Service3/>
+        <Brand1 />
         <Project2 />
         <Team2 />
-        <Pricing />
-        <CtaBanner2 />
+        {/* <Pricing /> */}
         <Achivements />
+        <CtaBanner2 />
         <Testimonial2 />
         <News2 />
       </Layout>
     </>
-  );
+  )
 }
--- app/page.js
+++ app/page.js
@@ -1,36 +1,4 @@
-// import Layout from "@/components/layout/Layout";
-// import About1 from "@/components/sections/About1";
-// import Brand1 from "@/components/sections/Brand1";
-// import Contact from "@/components/sections/Contact";
-// import CtaBanner from "@/components/sections/CtaBanner";
-// import Faq from "@/components/sections/Faq";
-// import Hero1 from "@/components/sections/Hero1";
-// import News1 from "@/components/sections/News1";
-// import OurSkills from "@/components/sections/OurSkills";
-// import Project1 from "@/components/sections/Project1";
-// import Service1 from "@/components/sections/Service1";
-// import Team1 from "@/components/sections/Team1";
-// import Testimonial1 from "@/components/sections/Testimonial1";
-// export default function Home() {
-//   return (
-//     <>
-//       <Layout headerStyle={1} footerStyle={1}>
-//         <Hero1 />
-//         <About1 />
-//         <Service1 />
-//         <CtaBanner />
-//         <OurSkills />
-//         <Testimonial1 />
-//         <Project1 />
-//         <Faq />
-//         <Team1 />
-//         <Contact />
-//         <News1 />
-//         <Brand1 />
-//       </Layout>
-//     </>
-//   );
-// }
+
 import Layout from "@/components/layout/Layout"
 import About2 from "@/components/sections/About2"
 import Achivements from "@/components/sections/Achivements"
@@ -38,9 +6,10 @@ import Brand1 from "@/components/sections/Brand1"
 import CtaBanner2 from "@/components/sections/CtaBanner2"
 import Hero2 from "@/components/sections/Hero2"
 import News2 from "@/components/sections/News2"
-import Pricing from "@/components/sections/Pricing"
+// import Pricing from "@/components/sections/Pricing"
 import Project2 from "@/components/sections/Project2"
-import Service2 from "@/components/sections/Service2"
+// import Service2 from "@/components/sections/Service2"
+import Service3 from "@/components/sections/Service3"
 import Team2 from "@/components/sections/Team2"
 import Testimonial2 from "@/components/sections/Testimonial2"
 export default function Home2() {
@@ -49,9 +18,10 @@ export default function Home2() {
       <Layout headerStyle={2} footerStyle={2}>
         <Hero2 />
         <About2 />
-        <Service2 />
+        {/* <Service2 /> */}
+        <Service3/>
         <Brand1 />
-        <Project2 />
+        {/* <Project2 /> */}
         <Team2 />
         {/* <Pricing /> */}
         <Achivements />
